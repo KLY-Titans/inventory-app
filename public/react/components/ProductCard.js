3e0ca40015126fe510cf88b1efb1377c0432b0dc
zluigon <luis.nmn.gonzalez@gmail.com>
2024-10-07 16:22:55 -0400

    Update: ProductList compoment initial implementation

--- public/react/components/ProductCard.js
+++ public/react/components/ProductCard.js
@@ -1,26 +1,19 @@
 import React from "react";
-import Grid from "@mui/material/Grid2";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
-export const ProductCard = ({product}) => {
-  const product = {
-    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  };
 
+const ProductCard = ({ product }) => {
   return (
     
-    // GRID FOR SPACING/LAYOUT
     <Card variant="outlined" sx={{ border: "solid 1px red", width: 400 }}>
       <CardHeader title={product.name} subheader={product.category} />
+      
+      {/* image needs uniform sizing */}
       <CardMedia component="img" src={product.image} />
       <CardContent>
         <Typography sx={{ marginBottom: 2 }} variant="body1">
@@ -29,7 +22,15 @@ export const ProductCard = ({product}) => {
         <Typography sx={{ marginBottom: 2 }} variant="h6">
           ${product.price}
         </Typography>
-      </CardContent>
+      </CardContent>{" "}
+
+      {/* needs conditional rendereing for single view only */}
+      <Stack spacing={2} direction="row">
+        <button>DELETE</button>
+        <button>EDIT</button>
+      </Stack>
     </Card>
   );
 };
+
+export default ProductCard;
